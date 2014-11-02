9aa65b275240676c7549db567a5e49be848c2356
Matt <info@mgates.me>
2014-11-02 11:57:47 -0500

    Auto 3 in 1

---
+++ Scripts/Auto3In1.lua
@@ -0,0 +1,64 @@
+--<< Phase boots, bottle in fountain, magic stick when low >>
+
+--===================--
+--     LIBRARIES     --
+--===================--
+require("libs.ScriptConfig")
+
+--===================--
+--      CONFIG       --
+--===================--
+config = ScriptConfig.new()
+config:SetParameter("Bottle", true)
+config:SetParameter("PhaseBoots", true)
+config:SetParameter("MagicStick", true)
+config:Load()
+
+local enableBottle     = config.Bottle
+local enablePhaseBoots = config.PhaseBoots
+local enableMagicStick = config.MagicStick
+local useStickAt       = 0.2 -- 20% HP
+
+--===================--
+--       CODE        --
+--===================--
+local target    = nil
+local sleepTick = nil
+
+function Tick( tick )
+	if not client.connected or client.loading or client.console or (sleepTick and sleepTick > tick) then
+		return
+	end
+
+	local me = entityList:GetMyHero()
+	if not me then return end Sleep(125)
+
+	local bottle     = me:FindItem("item_bottle")
+	local phaseboots = me:FindItem("item_phase_boots")
+	local lowstick   = me:FindItem("item_magic_stick")
+	local gradestick = me:FindItem("item_magic_wand")
+	local stick      = lowstick and lowstick or gradestick
+
+	if me.alive and not me:IsUnitState(LuaEntityNPC.STATE_INVISIBLE) and not me:IsChanneling() then
+
+		if enableBottle and bottle and me:DoesHaveModifier("modifier_fountain_aura_buff") and not me:DoesHaveModifier("modifier_bottle_regeneration") and (me.health < me.maxHealth or me.mana < me.maxMana) then
+			me:SafeCastItem(bottle.name)
+		end
+
+		if enablePhaseBoots and phaseboots then
+			me:SafeCastItem(phaseboots.name)
+		end
+
+		if enableMagicStick and stick and stick.charges > 0 and (me.health/me.maxHealth < useStickAt) then
+			me:SafeCastItem(stick.name)
+		end
+
+	end
+
+	-- Get visible allies
+	-- local allies = entityList:GetEntities({type=LuaEntity.TYPE_HERO, visible = true, alive = true, team = me:GetEnemyTeam(), illusion=false})
+
+	sleepTick = tick + 300
+end
+
+script:RegisterEvent(EVENT_TICK,Tick)
